4d9493ca8a746e35512e80db6552568248eeaa26
Kelley Danger <kelleyscanlon@gmail.com>
2024-07-24 23:24:19 -0400

    working basic website, yay

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type PageData struct {
+	Title       string
+	Name        string
+	Description string
+}
+
+// ---------------Page Handlers---------------
+func homepageHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Handler function called")
+	tmpl, err := template.ParseFiles("templates/base.html", "templates/index.html")
+	if err != nil {
+		log.Println("Error parsing templates:", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	data := PageData{
+		Title:       "About Me",
+		Name:        "Kelley & Sergio",
+		Description: "A brief description about the team behind Slurp.",
+	}
+	err = tmpl.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		log.Println("Error executing template:", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	//---------------Routers---------------
+	http.HandleFunc("/", homepageHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	//---------------Start Server---------------
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
