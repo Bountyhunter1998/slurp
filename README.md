72ee25aab4f858239a9a7573181943d799f6d95d
Kelley Scanlon <kelleyscanlon@gmail.com>
2024-11-05 21:00:54 -0500

    create-navigation-for-handlers

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Why: It's known for its performance, simplicity, and strong concurrency support.
 
 
 
-### [Temple](https://github.com/docwhat/temple)
+### [Temple](https://templ.guide/)
 What: Templating engine for Go.
 Why: It simplifies the process of rendering HTML pages dynamically.
 
@@ -31,32 +31,37 @@ Why: It allows you to add AJAX, CSS transitions, WebSockets, and more, directly 
 
 ## How To Install
 1. [Install Go](https://go.dev/dl/)
-2. [Install nvm-windows](https://github.com/coreybutler/nvm-windows?tab=readme-ov-file#install-nvm-windows)
-- [Install nvm-setup.exe](https://github.com/coreybutler/nvm-windows/releases)
-- ```console
-# install node.js
+2.  Install node.js
+```bash
 nvm install 22.5.1
 # after install, reinstall global utilities. 
 nvm use 22.5.1
 npm --version
 npm install -g yarn
 ```
-3. Initialize your go project by running the following in your bash terminal:
-- ```console
+3. Initialize your go project
+- ```bash
 # initialize your go project
 go mod init myproject
-
 ```
-4. Install Tailwindcss by running the following in your bash terminal:
-- `npm install -D tailwindcss`
+4. Install Tailwindcss
+```bash
+npm install -D tailwindcss
+```
 
-5. Set up Windows Defender Firewall to allow connections to localhost port 8080. [Follow this stack overflow answer.](https://stackoverflow.com/a/65393403)
-5. Run your go website by running the following in your bash terminal:
-- `go run main.go`
+5. Run your go website
+```bash
+go run main.go
+```
 
 6. Visit your webpage!
 - Open a web browser and go to [http://localhost:8080](http://localhost:8080)
 
+### Windows Specific
+1. Set up Windows Defender Firewall to allow connections to localhost port 8080. [Follow this stack overflow answer.](https://stackoverflow.com/a/65393403)
+2. [Install nvm-windows](https://github.com/coreybutler/nvm-windows?tab=readme-ov-file#install-nvm-windows)
+3. [Install nvm-setup.exe](https://github.com/coreybutler/nvm-windows/releases)
+
 ## Contributors
-- Kelley Danger
-- Sergio Sartini
+- [Kelley Danger](https://github.com/kelleyDanger)
+- [Sergio Sartini](https://github.com/Bountyhunter1998)
--- main.go
+++ main.go
@@ -14,25 +14,47 @@ type PageData struct {
 }
 
 // ---------------Page Handlers---------------
-func homepageHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Handler function called")
-	tmpl, err := template.ParseFiles("templates/base.html", "templates/index.html")
-	if err != nil {
-		log.Println("Error parsing templates:", err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
 
+// Parse all templates
+// var tmpl = template.Must(template.ParseGlob("templates/*.html"))
+
+// var tmpl *template.Template
+
+// // Initialize templates by parsing all HTML files once
+// func initTemplates() {
+//     var err error
+//     tmpl, err = template.ParseGlob("templates/*.html")
+//     if err != nil {
+//         log.Fatalf("Error parsing templates: %v", err)
+//     }
+// }
+
+// Render template with specified content template (e.g., "about.html")
+func renderTemplate(w http.ResponseWriter, tmplName string, data PageData) {
+	baseTemplate := "templates/base.html"
+    pageTemplate := fmt.Sprintf("templates/%s", tmplName)
+	tmpl, err := template.ParseFiles(baseTemplate, pageTemplate)
+    
+	err = tmpl.ExecuteTemplate(w, "base", data)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+func homepageHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Home handler function called")
 	data := PageData{
 		Title:       "Slurp",
 		Name:        "Kelley & Sergio",
 		Description: "A brief description about the team behind Slurp.",
 	}
-	err = tmpl.ExecuteTemplate(w, "base", data)
-	if err != nil {
-		log.Println("Error executing template:", err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-	}
+	// tmpl, err := template.ParseFiles("base.html", "index.html")
+	// err := tmpl.ExecuteTemplate(w, "base", data)
+	// if err != nil {
+	// 	log.Println("Error executing template:", err)
+	// 	http.Error(w, "Internal server error", http.StatusInternalServerError)
+	// }
+	renderTemplate(w, "index.html", data)
 }
 
 func messageHandler(w http.ResponseWriter, r *http.Request) {
@@ -40,12 +62,41 @@ func messageHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello from the server!")
 }
 
+func kelleyHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Kelley handler function called")
+
+	data := PageData{
+		Title: "About Kelley",
+	}
+	// err := tmpl.ExecuteTemplate(w, "base", data)
+	// if err != nil {
+	// 	log.Println("Error executing template for Kelley:", err)
+	// 	http.Error(w, "Internal server error", http.StatusInternalServerError)
+	// }
+	renderTemplate(w, "kelley.html", data)
+}
+
+func sergioHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Sergio handler function called")
+	data := PageData{
+		Title: "About Sergio",
+	}
+	renderTemplate(w, "sergio.html", data)
+	// err := tmpl.ExecuteTemplate(w, "templates/sergio.html", data)
+	// if err != nil {
+	// 	log.Println("Error executing template for Sergio:", err)
+	// 	http.Error(w, "Internal server error", http.StatusInternalServerError)
+	// }
+}
+
 func main() {
 	//---------------Routers---------------
-	http.HandleFunc("/", homepageHandler)
 	http.HandleFunc("/message", messageHandler)
+	http.HandleFunc("/kelley", kelleyHandler)
+    http.HandleFunc("/sergio", sergioHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
+	http.HandleFunc("/", homepageHandler)
 	//---------------Start Server---------------
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
